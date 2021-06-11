a38775a2672c5570d09a899945cbd7f752f6126a
Rafal1233 <85755709+Rafal1233@users.noreply.github.com>
2021-06-11 19:14:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,35 +1,38 @@
 const walec = document.querySelector('.walec')
 let style = getComputedStyle(walec)
-
+const end = document.querySelector('.end')
 position = style.getPropertyValue('right')
 positionvertical = style.getPropertyValue('top')
+let value = 50
 
 const handlewalec = (e) => {
     if (e.key == 'd') {
         walec.style.right = newposition - 10 + 'px'
-        newposition-=5
+        newposition -= 5
+
     }
-    if(e.key=='a'){
+    if (e.key == 'a') {
         walec.style.right = newposition - 10 + 'px'
-        newposition+=5
+        newposition += 5
     }
-    if(e.key=='w'){
+    if (e.key == 'w') {
         walec.style.top = newpositionvertical - 10 + 'px'
-       newpositionvertical-=5
+        newpositionvertical -= 5
     }
-    if(e.key=='s'){
+    if (e.key == 's') {
         walec.style.top = newpositionvertical - 10 + 'px'
-        newpositionvertical+=5
+        newpositionvertical += 5
+    }
+    if (newposition < -185) {
+        walec.style.transform = "rotate(45deg)"
+        end.classList.add('ending')
     }
 
 }
-const handle = () => {
-    walec.style.right = newposition + 10 + 'px'
-    newposition++
-}
+
 
 window.addEventListener('keydown', handlewalec)
 
 
 newposition = parseInt(position)
-newpositionvertical=parseInt(positionvertical)
+newpositionvertical = parseInt(positionvertical)
